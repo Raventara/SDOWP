702b8244afd85a22b1b76a61ded26a516fb0bc5d
Russell Bruechert <russell.bruechert@dtexsystems.com>
2015-02-25 15:46:11 +0000

    sort and organise usings

--- SDOWP/Form1.cs
+++ SDOWP/Form1.cs
@@ -1,15 +1,12 @@
 ﻿using AForge.Imaging.Filters;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
