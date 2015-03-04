bc3f95d05f21e231fe847cb35f145651c93792ce
Russell Bruechert <russell.bruechert@dtexsystems.com>
2015-03-04 10:43:26 +0000

    Created Settings class base

--- SDOWP/Settings.cs
+++ SDOWP/Settings.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace SDOW_P
 {
+	[Serializable]
 	class Settings
 	{
+		[Serializable]
+		public List<ScreenSetting> ScreenSettings { get; set; }
+	}
+
+	[Serializable]
+	public class ScreenSetting
+	{
+		[Serializable]
+		public string ScreenDeviceName { get; set; }
+		[Serializable]
+		public SDOSetting SDOSettings { get; set; }
+	}
+
+	[Serializable]
+	public class SDOSetting
+	{
+		[Serializable]
+		public List<bool> SDOSelections { get; set; }
+		[Serializable]
+		public bool Sliced { get; set; }
+		[Serializable]
+		public bool SlideShow { get; set; }
+		[Serializable]
+		public bool Random { get; set; }
 	}
 }
